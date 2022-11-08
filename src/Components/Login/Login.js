e5097b9444621c1de39dd74cc3093c82f9ca9523
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2022-11-08 12:37:00 -0800

    Login page is created

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -21,11 +21,11 @@ const Login = () => {
                             <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="" />
                         </div>
                         <div class="flex items-start">
-                            <Link href="#" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</Link>
+                            <Link to="#" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</Link>
                         </div>
                         <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
                         <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                            Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+                            Not registered? <Link to="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</Link>
                         </div>
                     </form>
                 </div>
