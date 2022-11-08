8274220db9a4eb9e0d51549a2570f4e092727e3f
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2022-11-08 04:30:12 -0800

    Banner responsive design complete

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -3,8 +3,8 @@ import BannerImage from '../../../Assets/Banner/banner-img.jpg';
 
 const Banner = () => {
     return (
-        <div className='container flex basis-2/4 my-8'>
-            <div className='w-1/2 flex flex-col justify-center'>
+        <div className='flex flex-col-reverse lg:flex-row lg:flex basis-2/4 my-8'>
+            <div className='w-full lg:w-1/2 flex flex-col justify-center'>
                 <div className="title">
                     <h1 className=" text-4xl leading-relaxed uppercase font-extrabold dark:text-white">Hi Mr. Guest</h1>
                     <h1 className=" text-3xl leading-relaxed uppercase font-extrabold text-yellow-400  dark:text-white">Delicious food for your taste</h1>
@@ -19,8 +19,8 @@ const Banner = () => {
                     <button type="button" class="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">Services</button>
                 </div>
             </div>
-            <div className="w-1/2">
-                <img src={BannerImage} alt="banner" />
+            <div className=" w-full lg:w-1/2">
+                <img className='w-full h-full' src={BannerImage} alt="banner" />
             </div>
         </div>
     );
